14eaa56ddc824b59d2b1b9abacc00c876d3e5dfc
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-18 02:05:39 +0700

    update docker-compose

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,1 +1,8 @@
+# before deploying
+docker login quay.io
+gcloud auth login
+
 docker-compose up -d prometheus grafana 
+
+# MEET THIS PROBLEM: error getting credentials - err: exit status 1, out: ''
+# In ~/.docker/config.json change credsStore to credStore
